3715f4f5e3512724e52403658770d5b1c5f09b59
Evan Sellers <sellersew@gmail.com>
2021-03-13 15:18:43 -0500

    floating nav bar

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if ( scroll >= 100 ) {
+        $( ".floating-nav" ).addClass( "scrolled" );
+    } else {
+        $( ".floating-nav" ).removeClass( "scrolled" );
+    }
+});
+
+
+// add sticky header only on desktop
