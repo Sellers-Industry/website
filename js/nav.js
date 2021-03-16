b9c667fcb0de921c8ec3b7eeefd74608d7bd5511
Evan Sellers <sellersew@gmail.com>
2021-03-16 09:10:13 -0400

    copyright headers

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,17 @@
+/*
+ *   Copyright (C) 2021 Sellers Industry - All Rights Reserved
+ *   Unauthorized copying of this file, via any medium is strictly
+ *   prohibited. Proprietary and confidential.
+ *
+ *   author: Evan Sellers <sellersew@gmail.com>
+ *   date: Tue Mar 16 2021
+ *   file: nav.js
+ *   project: Sellers Industry Website
+ *   purpose: Navigation Scroll
+ *
+ */
+
+
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
